392e3f0578535ccf8e83245107849d3f8f03f82d
Daryaaa <daryaaagulevich@gmail.com>
2017-07-20 14:05:10 +0200

    DEV-3

---
+++ IsFibonacci.java
@@ -0,0 +1,33 @@
+public class IsFibonacci {
+  public static String IsNumberFibonacci(int n) {
+    String result = null;
+    int firstFibonacci = 0;
+    int secondFibonacci = 1;
+    if ( n < firstFibonacci ){
+      result = "Illegal input";
+    } else  if (n == firstFibonacci || n == secondFibonacci) {
+      result = "Is Fibonacci";
+    } else if (n > secondFibonacci){
+      int thirdFibonacci = 1;
+      for (int sum = 0; sum < n; sum++) {
+        sum = secondFibonacci+thirdFibonacci;
+        secondFibonacci = thirdFibonacci;
+        thirdFibonacci = sum;
+        if (sum == n) {
+          result = "Is Fibonacci";
+        } else if (sum > n){
+          result = "Isn't Fibonacci";
+        }
+      }
+    } System.out.println(result);
+      return result;
+  }
+  public static void main(String[] args) throws IOException {
+    InputStream inputStream = System.in;
+    Reader inputStreamReader = new InputStreamReader(inputStream);
+    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+    String sn = bufferedReader.readLine();
+    int n = Integer.parseInt(sn);
+    IsNumberFibonacci(n);
+  }
+}
