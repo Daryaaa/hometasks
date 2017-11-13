9dad95fb3d045b4d91f45ee5cd067938af899106
Daryaaa <daryaaagulevich@gmail.com>
2017-11-13 02:46:08 +0200

    small changes to revert DEV-11 to its original state

--- DV-11/src/EnterPoint.java
+++ DV-11/src/EnterPoint.java
@@ -2,7 +2,7 @@ public class EnterPoint {
   public static void main(String[] args) {
     Transliteration example = new Transliteration();
     try{
-      System.out.println(example.convert("щит"));
+      System.out.println(example.convert("ghjfhhg"));
     } catch (IllegalArgumentException e) {
       System.out.println("String is empty");
     }
